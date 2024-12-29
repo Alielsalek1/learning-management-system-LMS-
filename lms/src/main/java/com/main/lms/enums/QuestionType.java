eaaaa0fa8c37c03df9a1d6be525edc270657698c
Ali Abdelrahman <117834271+Alielsalek1@users.noreply.github.com>
2024-12-29 23:46:28 +0200

    added file

---
+++ lms/src/main/java/com/main/lms/enums/QuestionType.java
@@ -0,0 +1,7 @@
+package com.main.lms.enums;
+
+public enum QuestionType {
+    MCQ,        // Multiple Choice Question
+    TRUE_FALSE, // True or False Question
+    SHORT_ANSWER; // Short Answer
+}
